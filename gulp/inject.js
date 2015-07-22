407748ae9d593dbb347b42b89c2fb24c057d69be
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-07-22 10:12:16 +0200

    Styles moved to app/styles/

--- gulp/inject.js
+++ gulp/inject.js
@@ -11,16 +11,17 @@ var ext_replace = require('gulp-ext-replace');
 
 gulp.task('inject', 'Inject styles and scripts into html', [], function () {
   var injectStyles = gulp.src([
-    path.join(conf.path.src, '/css/*.css'),
+    path.join(conf.path.src, 'components/**/*.css'),
+    path.join(conf.path.src, 'styles/*.css'),
   ], { read: false });
 
   var injectScripts = gulp.src([
     path.join(conf.path.src, 'components/**/*.js'),
-    path.join('!' + conf.path.src, '/**/*.spec.js'),
-    path.join('!' + conf.path.src, '/**/*.mock.js')
+    path.join('!' + conf.path.src, '**/*.spec.js'),
+    path.join('!' + conf.path.src, '**/*.mock.js')
   ]).pipe(angularFilesort());
 
-  return gulp.src(path.join(conf.path.src, '/*.src.html'))
+  return gulp.src(path.join(conf.path.src, '*.src.html'))
     .pipe(inject(injectStyles, {relative: true}))
     .pipe(inject(injectScripts, {relative: true}))
     .pipe(inject(gulp.src(bowerFiles({includeDev:true}), {read: false}), {relative: true, name: 'bower'}))
