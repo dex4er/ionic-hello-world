c57a065b59ee42531d9b86d82bbb222cdd5df7e4
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-07-29 22:55:27 +0200

    gulp cordova:update

--- gulp/cordova.js
+++ gulp/cordova.js
@@ -3,6 +3,7 @@
 var conf = require('./conf');
 
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 
 var runSequence = require('run-sequence');
 
@@ -11,11 +12,23 @@ var runSequence = require('run-sequence');
     var cordova = require('../platforms/' + platform + '/cordova/lib/build');
     cordova.runClean()
       .then(done);
-  });  
+  });
+
+  gulp.task('cordova:update:' + platform, "Update Cordova platform " + platform, function(done) {
+    gulp.src('')
+      .pipe($.shell('cordova platform update ' + platform))
+      .on('end', done)
+  });
 });
 
 gulp.task('clean:cordova', "Clean all Cordova project folders", function(done) {
   runSequence(conf.cordova.platforms.map(function(platform) {
     return 'clean:cordova:'+platform;
+  }), done);
+});
+
+gulp.task('cordova:update', "Update all Cordova platforms", function(done) {
+  runSequence(conf.cordova.platforms.map(function(platform) {
+    return 'cordova:update:'+platform;
   }), done);
 });
