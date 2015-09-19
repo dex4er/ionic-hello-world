cc25f01347e572fb6dc18751ebdd76fc48bfe57a
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-09-19 16:08:19 +0200

    Backbutton returns to tab.dash state and then exits

--- src/app/backbutton.run.js
+++ src/app/backbutton.run.js
@@ -1,14 +1,20 @@
 'use strict';
 
-angular.module('app').run(function($ionicHistory, $ionicPlatform) {
-  $ionicPlatform.registerBackButtonAction(function(event) {
-    console.log($ionicHistory.currentStateName());
-    if (($ionicHistory.currentStateName().match(/\./g)||[]).length <= 1) {
-      ionic.Platform.exitApp();
-    } else {
-      $ionicHistory.goBack();
-    }
-    event.preventDefault();
-    return false;
-  }, 101);
+angular.module('app').run(function($ionicHistory, $ionicPlatform, $state) {
+  $ionicPlatform.ready(function() {
+    $ionicPlatform.registerBackButtonAction(function(event) {
+      console.log($ionicHistory.currentStateName());
+      if (($ionicHistory.currentStateName().match(/\./g)||[]).length <= 1) {
+        if ($ionicHistory.currentStateName() === 'tab.dash') {
+          ionic.Platform.exitApp();
+        } else {
+          $state.go('tab.dash');
+        }
+      } else {
+        $ionicHistory.goBack();
+      }
+      event.preventDefault();
+      return false;
+    }, 101);
+  });
 });
