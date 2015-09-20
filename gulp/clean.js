6491541ddbe29d6457ae3e969540ae8eecc03b55
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-09-20 14:29:37 +0200

    Add cordova:clean to clean target

--- gulp/clean.js
+++ gulp/clean.js
@@ -7,14 +7,14 @@ var gulp = require('gulp');
 var del = require('del');
 var runSequence = require('run-sequence');
 
-gulp.task('clean:tmp', "Clean temporary folder", function (done) {
-  del(conf.paths.tmp, done);
+gulp.task('clean:tmp', "Clean temporary folder", function() {
+  return del(conf.paths.tmp);
 });
 
-gulp.task('clean:dest', "Clean dest folder", function (done) {
-  del(conf.paths.dest, done);
+gulp.task('clean:dest', "Clean dest folder", function() {
+  return del(conf.paths.dest);
 });
 
 gulp.task('clean', "Clean all folders", function(done) {
-  runSequence('clean:dest', 'clean:tmp', done);
+  runSequence('clean:dest', 'clean:tmp', 'cordova:clean', done);
 });
