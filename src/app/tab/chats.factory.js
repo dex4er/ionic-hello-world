478da9fdee47654d92c9b142f40cf6004bbf7ac1
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2016-01-01 19:07:48 +0100

    Offline versions for avatars

--- src/app/tab/chats.factory.js
+++ src/app/tab/chats.factory.js
@@ -8,27 +8,27 @@ angular.module('app').factory('Chats', function ChatsFactory() {
     id: 0,
     name: 'Ben Sparrow',
     lastText: 'You on your way?',
-    face: 'http://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    face: 'img/9SgAuHeY.png'
   }, {
     id: 1,
     name: 'Max Lynx',
     lastText: 'Hey, it\'s me',
-    face: 'http://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    face: 'img/11214.jpeg'
   }, {
     id: 2,
     name: 'Adam Bradleyson',
     lastText: 'I should buy a boat',
-    face: 'http://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+    face: 'img/84TKj_qa.jpeg'
   }, {
     id: 3,
     name: 'Perry Governor',
     lastText: 'Look at my mukluks!',
-    face: 'http://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+    face: 'img/3j5HUXMY.png'
   }, {
     id: 4,
     name: 'Mike Harrington',
     lastText: 'This is wicked good ice cream.',
-    face: 'http://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+    face: 'img/R3ae1n61.png'
   }];
 
   return {
